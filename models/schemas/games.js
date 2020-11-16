0d776f64068a4ebe4f3f6605759192f8d734e94a
Joe Bentley <joe.bentley.20@gmail.com>
2020-11-16 16:30:07 +0000

    #10 Added Schema files
    Added keys to games.js

---
+++ models/schemas/games.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const gameSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    ageRating: {
+        type: String,
+        required: true
+    },
+    violence: {
+        type: String,
+        required: true,
+    },
+    sexAndNudity: {
+        type: String,
+        required: true,
+    },
+    alcoholAndDrugs: {
+        type: String,
+        required: true,
+    },
+    gambling: {
+        type: String,
+        required: true,
+    },
+    explicitLanguage: {
+        type: String,
+        required: true,
+    },
+    averageScore: {
+        type: String,
+        required: true,
+    },
+    publisher: {
+        type: String,
+        required: true,
+    },
+    releaseDate: {
+        type: Date,
+        required: true,
+    }
+});
+module.exports = mongoose.model('games', gameSchema);
