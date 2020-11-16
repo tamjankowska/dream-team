c08bcb46aeaeac7cbf1da2a27bf4de2bdbc9446c
Madeline <madeline.harrop@gmail.com>
2020-11-16 15:25:04 +0000

    basic signup.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const session = require("express-session")
 
 app.use(cors());
+app.session(session({resave:true, saveUninitialized: true, secret:"asdf"}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
