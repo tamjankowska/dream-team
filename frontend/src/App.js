b1b8580a6b3411e98c545c59075ee39556b9c2af
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2020-11-17 16:56:39 +0000

    #13 Carousel Banner component plus imported extra images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Signup from "./components/signup/Signup"
+import Signup from "./components/signup/Signup";
+import GamesBanner from './components/gamesbanner/GamesBanner';
 
 function App() {
   return (
     <div className="App">
+      <GamesBanner />
       <Signup />
     </div>
   );
