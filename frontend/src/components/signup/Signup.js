eacb21ecf1948bf7e08862e17f201d09968ec236
Madeline <madeline.harrop@gmail.com>
2020-11-17 11:47:42 +0000

    Sign-up page
    Sign-up linked to "/"

--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,29 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-const Signup = (props) => {
-    const [data, setData] = useState('');
-    const getResponse = () => {
-        axios.get('http://localhost:5000/').then((res) => {
-            setData(res.data.testing);
+const Signup = () => {
+    const [username, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordCheck, setPasswordCheck] = useState("");
+
+    const getResponse = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/signup', {
+            username: username,
+            email: email,
+            password: password,
+            passwordCheck: passwordCheck
+        }).then((res) => {
+            console.log(res.data)
         })
     };
-    const clearData = () => {
-        console.log('Clearing');
-        setData('');
-    };
-
+    const onChangeHandler = (event) => {
+        if (event.target.id === "username") {
+            setUser(event.target.value)
+        } else if (event.target.id === "email") {
+            setEmail(event.target.value)
+        } else if (event.target.id === "password") {
+            setPassword(event.target.value)
+        } else if (event.target.id === "passwordCheck") {
+            setPasswordCheck(event.target.value)
+        }
+    }
     return (
-        <div className = "signup">
+        <div className="signup">
             <h1>Sign Up</h1>
-            <form>
-            <input type="text" name="username" placeholder="username"></input>
+            <form onSubmit={getResponse}>
+                <ul>
+                    <label htmlFor="username">Create a username</label>
+                    <input id="username" type="text" name="username" placeholder="username" onChange={onChangeHandler}></input>
+
+                    <label htmlFor="email">Enter an email</label>
+                    <input id="email" type="email" name="email" placeholder="email" onChange={onChangeHandler}></input>
+
+                    <label htmlFor="password">Create a password</label>
+                    <input id="password" type="text" name="password" placeholder="password" onChange={onChangeHandler}></input>
+
+                    <label htmlFor="passwordCheck">Re-enter the password</label>
+                    <input id="passwordCheck" type="text" name="passwordCheck" placeholder="passwordCheck" onChange={onChangeHandler}></input>
 
 
-            <input type="submit"></input>
+                    <input type="submit" value="submit"></input>
+                </ul>
             </form>
-            <button onClick = {getResponse}>Get Data</button>
-            <button onClick = {clearData}>Clear Data</button>
         </div>
     );
 }
