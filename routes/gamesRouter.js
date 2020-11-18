daa428c77c5d94b2c53cebc99a9233880097b951
Tam Jankowska <tam.jankowska@gmail.com>
2020-11-18 17:38:39 +0000

    #18 updated games router with various post requests
    The delete request is not yet working but the structure is there.

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -12,11 +12,87 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req, res) => {
+router.post('/add', (req, res) => {
     const title = req.body.title;
     const category = req.body.category;
-    new Game({ title, category }).save((err, result) => {
-        res.status(200).send({ status: "testing", result });
+    const ageRating = req.body.ageRating;
+    const violence = req.body.violence;
+    const sexAndNudity = req.body.sexAndNudity;
+    const alcoholAndDrugs = req.body.alcoholAndDrugs;
+    const gambling = req.body.gambling;
+    const explicitLanguage = req.body.explicitLanguage;
+    const averageScore = req.body.averageScore;
+    const publisher = req.body.publisher;
+    const releaseDate = req.body.releaseDate;
+    new Game({ title, 
+        category, 
+        ageRating, 
+        violence, 
+        sexAndNudity, 
+        alcoholAndDrugs, 
+        gambling, 
+        explicitLanguage,
+        averageScore,
+        publisher,
+        releaseDate}).save((err, result) => {
+        res.status(200).send({ status: result });
+    });
+});
+
+router.post('/search', (req, res) => {
+    const title = req.body.title;
+    const category = req.body.category;
+    const ageRating = req.body.ageRating;
+    const violence = req.body.violence;
+    const sexAndNudity = req.body.sexAndNudity;
+    const alcoholAndDrugs = req.body.alcoholAndDrugs;
+    const gambling = req.body.gambling;
+    const explicitLanguage = req.body.explicitLanguage;
+    const averageScore = req.body.averageScore;
+    const publisher = req.body.publisher;
+    const releaseDate = req.body.releaseDate;
+    const query = {};
+    if (title) {
+        query.title = title;
+    }
+    if (category) {
+        query.category = category;
+    }
+    if (ageRating) {
+        query.ageRating = ageRating;
+    }
+    if (violence) {
+        query.violence = violence;
+    }
+    if (sexAndNudity) {
+        query.sexAndNudity = sexAndNudity;
+    }
+    if (alcoholAndDrugs) {
+        query.alcoholAndDrugs = alcoholAndDrugs;
+    }
+    if (gambling) {
+        query.gambling = gambling;
+    }
+    if (explicitLanguage) {
+        query.explicitLanguage = explicitLanguage;
+    }
+    if (averageScore) {
+        query.averageScore = averageScore;
+    }
+    if (publisher) {
+        query.publisher = publisher;
+    }
+    if (releaseDate) {
+        query.releaseDate = releaseDate;
+    }
+
+    Game.find(query, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(204).json([]);
+        } else {
+        res.status(200).json(results);
+        }
     });
 });
 
@@ -27,6 +103,17 @@ router.get('/id/:id', (req, res) => {
             res.status(404).json({err: `The game ${req.params.id} does not exist!`});  
         } else {
             res.status(200).json(game);
+        }
+    });
+});
+
+router.delete('/delete', (req, res) => {
+    Game.findByIdAndDelete({_id: req.params.id}, (err, game) => {
+        if (err) {
+            console.log(err);
+            res.status(404).json({err: `The game '${req.params.id}' you are trying to delete does not exist!`});
+        } else {
+            res.status(204).json(game);
         }
     });
 });
