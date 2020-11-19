ece414849b2fdedf73050d78d84483748bf525b2
Madeline <madeline.harrop@gmail.com>
2020-11-19 15:03:15 +0000

    app changes to be latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,7 @@ import Landing from "./components/landing/Landing";
 import { BrowserRouter, Route } from 'react-router-dom';
 import Signup from "./components/signup/Signup";
 import Navbar from "./components/navbar/Navbar";
-import Homepage from "./components/homepage/HomePage";
-import Profile from "./components/profile/Profile"
-// import GamesBanner from './components/gamesbanner/GamesBanner';
+import HomePage from './components/homepage/HomePage';
 
 function App() {
   return (
@@ -14,13 +12,11 @@ function App() {
       <div className="navbar">
         <Navbar />
       </div>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Route exact path="/" component={Landing} />
         <Route exact path="/signup" component={Signup} />
-      </BrowserRouter>
-      <Homepage />
-      <br></br> */}
-      <Profile />
+        <Route exact path="/home" component={HomePage} />
+      </BrowserRouter> 
     </div>
   );
 }
