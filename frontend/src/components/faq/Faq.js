0d3b7d37268ce484fd14919e8b812352f5a838d2
Joe Bentley <joe.bentley.20@gmail.com>
2020-11-19 16:14:19 +0000

    #25 Created + Linked FAQ component
    use /FAQ to access

---
+++ frontend/src/components/faq/Faq.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../faq/Faq.css';
+
+const FAQ = () => {
+    return (
+        <div className="faq-layout">
+            <h2>TEST</h2>
+        </div>
+    );
+};
+export default FAQ;
